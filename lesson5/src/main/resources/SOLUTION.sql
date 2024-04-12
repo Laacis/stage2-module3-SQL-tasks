024e62a80c9600e1be0c475c44078422407f1348
Laacis <109668309+Laacis@users.noreply.github.com>
2024-04-12 21:42:28 +0800

    added wildcard to name Mike

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -5,7 +5,7 @@ SELECT * FROM student
     WHERE groupnumber = 10
     AND birthday >= ADDDATE(CURRENT_DATE(), INTERVAL -20 YEAR);
 SELECT * FROM student
-    WHERE name ='Mike'
+    WHERE name LIKE 'Mike%'
     OR groupnumber BETWEEN 4 AND 6;
 SELECT * FROM payment
     WHERE payment_date >= ADDDATE(CURRENT_DATE(), INTERVAL -8 MONTH);
