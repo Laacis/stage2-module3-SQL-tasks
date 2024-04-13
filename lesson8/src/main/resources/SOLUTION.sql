abcb99675a10d7591c8851c56901224f0ebf5f54
Laacis <109668309+Laacis@users.noreply.github.com>
2024-04-13 09:32:44 +0800

    minor syntax fixes

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
-SELECT * FROM student WHERE birthday = (SELECT MIN(birthday) FROM student);
-SELECT * FROM payment WHERE payment_date = (SELECT MIN(payment_date) FROM payment);
+SELECT * FROM student WHERE birthday = (SELECT MAX(birthday) AS max_birth_day FROM student);
+SELECT * FROM payment WHERE payment_date = (SELECT MIN(payment_date) AS min_pay_date FROM payment);
 SELECT AVG(m.mark) FROM mark m
     JOIN subject sub ON m.subject_id=sub.id
     WHERE sub.name='Math';
