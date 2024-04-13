a816f8b626790c87b4b355a8a129b055ea31a627
Laacis <109668309+Laacis@users.noreply.github.com>
2024-04-13 08:08:42 +0800

    minor changes

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,15 +1,21 @@
 SELECT * FROM payment WHERE amunt >= 500;
+
 SELECT * FROM student
          WHERE birthday <= ADDDATE(CURRENT_DATE(), INTERVAL -20 YEAR);
+
 SELECT * FROM student
     WHERE groupnumber = 10
     AND birthday >= ADDDATE(CURRENT_DATE(), INTERVAL -20 YEAR);
+
 SELECT * FROM student
     WHERE name LIKE 'Mike%'
     OR groupnumber BETWEEN 4 AND 6;
+
 SELECT * FROM payment
     WHERE payment_date >= ADDDATE(CURRENT_DATE(), INTERVAL -8 MONTH);
+
 SELECT * FROM student WHERE name LIKE 'A%';
+
 SELECT * FROM student
     WHERE (name='Roxi' AND groupnumber=4)
     OR (name='Tallie' AND groupnumber=9);
