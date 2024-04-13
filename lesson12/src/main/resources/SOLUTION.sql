6ce42a3dff777e3502be8ac9b298ef4595bc2e03
Laacis <109668309+Laacis@users.noreply.github.com>
2024-04-13 15:26:55 +0800

    ALTER tables removed

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -10,11 +10,25 @@ DELETE FROM payment WHERE student_id IN (
 );
 DELETE FROM student WHERE grade >= 4;
 
+DELETE FROM payment WHERE student_id IN (
+    SELECT student_id
+    FROM mark
+    WHERE mark >= 4
+);
+DELETE FROM student WHERE id IN (
+    SELECT student_id
+    FROM mark
+    WHERE mark >= 4
+);
+DELETE FROM mark WHERE mark < 4;
+
+
 DELETE FROM payment WHERE type_id IN (
     SELECT id
     FROM paymenttype
     WHERE name = 'DAILY'
 );
+
 DELETE FROM paymenttype WHERE  name = 'DAILY';
 
 DELETE FROM mark WHERE mark < 7;
