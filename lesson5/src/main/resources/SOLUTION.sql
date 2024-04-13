5666d7bffc2cf626128cc5ccdc668b1713059101
Laacis <109668309+Laacis@users.noreply.github.com>
2024-04-13 08:57:09 +0800

    ADDDATE to DATEADD

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,18 +1,18 @@
 SELECT * FROM payment WHERE amunt >= 500;
 
 SELECT * FROM student
-         WHERE birthday <= ADDDATE(CURRENT_DATE(), INTERVAL -20 YEAR);
+         WHERE birthday <= DATEADD(CURRENT_DATE(), INTERVAL -20 YEAR);
 
 SELECT * FROM student
     WHERE groupnumber = 10
-    AND birthday >= ADDDATE(CURRENT_DATE(), INTERVAL -20 YEAR);
+    AND birthday >= DATEADD(CURRENT_DATE(), INTERVAL -20 YEAR);
 
 SELECT * FROM student
     WHERE name LIKE 'Mike%'
     OR groupnumber BETWEEN 4 AND 6;
 
 SELECT * FROM payment
-    WHERE payment_date >= ADDDATE(CURRENT_DATE(), INTERVAL -8 MONTH);
+    WHERE payment_date >= DATEADD(CURRENT_DATE(), INTERVAL -8 MONTH);
 
 SELECT * FROM student WHERE name LIKE 'A%';
 
